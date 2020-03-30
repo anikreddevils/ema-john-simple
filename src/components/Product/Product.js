904c31716e93a97f53914ced8aea18828630efc0
anikreddevils <anikreddevils@gmail.com>
2020-03-30 11:25:48 +0600

    upadte fakedata

---
+++ src/components/Product/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Product = (props) => {
+    console.log(props.product);
+    return (
+        <div>
+            <h3>hiii</h3>
+            
+        </div>
+    );
+};
+
+export default Product;
